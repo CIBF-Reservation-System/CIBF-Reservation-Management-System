557a2f4fbed379e103bb9f265bfd9ebe372144e5
Malshi-Gimnadhi <malshigimnadhi@gmail.com>
2025-10-27 14:59:58 +0530

    Add Terms and Conditions page

--- NewFrontend/src/App.tsx
+++ NewFrontend/src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,7 +8,9 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Reserve from "./pages/Reserve";
 import Bookings from "./pages/Bookings";
+import Genres from "./pages/Genres";
 import OrganizerDashboard from "./pages/OrganizerDashboard";
+import Terms from "./pages/Terms";
 import { ConfirmationSuccess } from "./components/ConfirmationSuccess";
 import NotFound from "./pages/NotFound";
 
@@ -43,6 +46,8 @@ const App = () => (
           <Route path="/auth" element={<Auth />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/bookings" element={<Bookings />} />
+          <Route path="/genres" element={<Genres />} />
+          <Route path="/terms" element={<Terms />} />
           <Route path="/confirmation" element={<ConfirmationRoute />} />
           <Route path="/organizer" element={<OrganizerDashboard />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
