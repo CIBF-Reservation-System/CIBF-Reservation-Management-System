3ad98808ec9938eb6ae724d68b16251413068b1d
Chanuka17 <117577521+Chanuka17@users.noreply.github.com>
2025-11-07 08:15:02 +0530

    add notification types

---
+++ Backend/notification-service/src/main/java/com/cibf/notificationservice/notification/model/enums/NotificationType.java
@@ -0,0 +1,8 @@
+package lk.bookfair.notification.model.enums;
+
+public enum NotificationType {
+    RESERVATION_CONFIRMATION,
+    REGISTRATION_CONFIRMATION,
+    PASSWORD_RESET,
+    ADMIN_ALERT
+}
